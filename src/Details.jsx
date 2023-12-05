e85a55bcb71fd4a35c237a6798686ab4a52e5eff
PDochev <plamendochev0@gmail.com>
2023-12-05 14:50:49 +0000

    details responsive

--- src/Details.jsx
+++ src/Details.jsx
@@ -7,7 +7,7 @@ export default function Details() {
       <h1 id="details">Details</h1>
       <div className="details">
         <div className="det-img">
-          <img src="./src/assets/det2.jpg" />
+          <img src="./src/assets/det1.png" />
         </div>
         <div className="DetailText">
           <h2>SEASON 12</h2>
@@ -30,7 +30,21 @@ export default function Details() {
           </p>
         </div>
         <div className="det-img">
-          <img src="./src/assets/det3.jpg" />
+          <img src="./src/assets/det2.jpeg" />
+        </div>
+      </div>
+      <div className="details">
+        <div className="det-img">
+          <img src="./src/assets/det2.jpg" />
+        </div>
+        <div className="DetailText">
+          <h2>SEASON 12</h2>
+          <p>
+            This Season, a mysterious hacktivist named Zero-Day has left the
+            shadows, taking control of Neo Tokyo Arena. Find your place in a
+            world of digital decay with Rocket Pass Premium and earn dozens of
+            new items, including the Porsche 911 Turbo.
+          </p>
         </div>
       </div>
     </div>
