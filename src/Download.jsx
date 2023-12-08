e7256d32f58688f728a1faaae590380a9f2e5a3e
PDochev <plamendochev0@gmail.com>
2023-12-08 18:54:08 +0000

    download link fix

--- src/Download.jsx
+++ src/Download.jsx
@@ -17,47 +17,45 @@ const lightModeStyle = {
 
 export default function Download({ lightMode }) {
   return (
-    <div className="Container">
-      <div className="Download">
-        <h1 id="download">Download</h1>
-        <div className="downloads">
-          <div className="steam">
-            <img loading="lazy" src={steamPic} alt="steam logo image" />
-            <div className="descDownload">
-              <p>
-                You can buy the game from the link below. The game is available
-                for Meta Quest 2 on Steam.
-              </p>
-              <a
-                aria-label="Download"
-                href="https://store.steampowered.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button style={lightMode ? lightModeStyle : darkModeStyle}>
-                  Download
-                </button>
-              </a>
-            </div>
+    <div id="download" className="Download">
+      <h1>Download</h1>
+      <div className="downloads">
+        <div className="steam">
+          <img loading="lazy" src={steamPic} alt="steam logo image" />
+          <div className="descDownload">
+            <p>
+              You can buy the game from the link below. The game is available
+              for Meta Quest 2 on Steam.
+            </p>
+            <a
+              aria-label="Download"
+              href="https://store.steampowered.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button style={lightMode ? lightModeStyle : darkModeStyle}>
+                Download
+              </button>
+            </a>
           </div>
-          <div className="mega">
-            <img loading="lazy" src={megaPic} alt="mega logo image" />
-            <div className="descDownload">
-              <p>
-                You can download a demo of the game from the link below. The
-                game is available on MEGA.
-              </p>
-              <a
-                aria-label="Download"
-                href="https://mega.nz/file/54YThKDI#HxyMxfFwSF9LxjfoTwx935lI0KMAEI5EOYIwYhDKKAc"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button style={lightMode ? lightModeStyle : darkModeStyle}>
-                  Download
-                </button>
-              </a>
-            </div>
+        </div>
+        <div className="mega">
+          <img loading="lazy" src={megaPic} alt="mega logo image" />
+          <div className="descDownload">
+            <p>
+              You can download a demo of the game from the link below. The game
+              is available on MEGA.
+            </p>
+            <a
+              aria-label="Download"
+              href="https://mega.nz/file/54YThKDI#HxyMxfFwSF9LxjfoTwx935lI0KMAEI5EOYIwYhDKKAc"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button style={lightMode ? lightModeStyle : darkModeStyle}>
+                Download
+              </button>
+            </a>
           </div>
         </div>
       </div>
